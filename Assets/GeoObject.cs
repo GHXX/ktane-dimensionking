d5d4af44fcc60c46e36da27de41f9be76a52ac4f
GHXX <GHXX@gmx.at>
2020-05-15 14:56:58 +0200

    added some base code

---
+++ Assets/GeoObject.cs
@@ -0,0 +1,40 @@
+﻿using TheNCube;
+using UnityEngine;
+
+namespace Assets
+{
+    internal class GeoObject
+    {
+        VertexObject[] VertexLocations;
+        VertexObject[][] FacesToVert
+
+        
+
+        public GeoObject()
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+        internal class VertexObject
+        {
+            PointND Position;
+            KMSelectable ModuleVertex;
+        }
+        
+        internal class FaceObject
+        {
+            internal int[] verticeIds;
+            internal VertexObject[] vertexObjects;
+            internal MeshFilter face;
+        }
+    }
+}
---
+++ Assets/SchlafliInterpreter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets
+{
+    static class SchlafliInterpreter
+    {
+        internal   static GeoObject InterpretAndCompute()
+        {
+
+        }
+    }
+}
